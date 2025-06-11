1db1c6693d62d69f7c85f195658ac20130eba5d0
david-aravena <david.aravenap@gmail.com>
2025-06-10 21:19:02 -0400

    update: Header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,10 +7,17 @@ export default function Header({ padding }) {
   const location = useLocation();
 
   return (
-    <div style={{ display: "flex", flexDirection: "row", justifyContent:"space-between", alignItems:"center", width:"100%", padding: padding, gap: "1rem", backgroundColor:"white"}}>
+    <div style={{ display: "flex", flexDirection: "row", justifyContent:"flex-end", alignItems:"center", width:"100%", padding: padding, gap: "1rem", backgroundColor:"rgb(76, 8, 75)", height:"150px"}}>
 
-      <div>
-        <div style={{width:"150px", height:"auto"}}>
+      <div style={{position:"absolute", top:15, left:15}}>
+        <div style={{
+          width:"150px", 
+          height:"auto", 
+          background:"white", 
+          borderRadius:"150px", 
+          border:"15px solid rgb(76, 8, 75)",
+          padding:"10px",
+        }}>
           <Link to={"/"}>
             <img
               src="/icons/LogoTefi.png"
@@ -18,13 +25,13 @@ export default function Header({ padding }) {
               style={{ width: '100%', height: '100%', objectFit: 'contain' }}
             />
           </Link>
-        </div>
-      </div>
+        </div>   
+      </div> 
 
       {location.pathname !== "/quienes-somos" && (
         <div>
           <Link to={"/quienes-somos"}>
-            <h2>Quienes somos</h2>
+            <h2 style={{fontSize:"20px", color:"white"}}>Quienes somos</h2>
           </Link>
         </div>
       )}
